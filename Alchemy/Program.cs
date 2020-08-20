33e70e55b53cb1ed1dad5e51d25e55396cb31b53
Frederic Crete <1747302@cstjean.qc.ca>
2020-08-20 15:08:54 -0400

    Add project files.

---
+++ Alchemy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alchemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AlchemyLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AlchemyLib
+{
+    public class Class1
+    {
+    }
+}
